13e629942ed82e0415b3a41212f234548a234a27
mdwrds14 <48966382+mdwrds14@users.noreply.github.com>
2019-10-29 08:48:13 -0500

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -1,31 +1,31 @@
 #The calculations methods will reside here.
 #Let's get this bread
 
-def calcWinner(team1Score, team2Score, betLine):
+def calc_Winner(team1Score, team2Score, betLine):
     return team1Score > (team2Score + betLine)
 
-def calcUserPoints(userChoice, team1Score, team2Score, betLine):
+def calc_User_Points(userChoice, team1Score, team2Score, betLine):
     if(userChoice == team1 and calcWinner(team1Score, team2Score, betline)):
         userPoint += 1
     return userPoint
 
-def UserBetPoints(SeasonUserWin,UserScore,WeeklyUserWin,SeasonUser,WeeklyUser,WeeklyUserGOWWin,SeasonUserGOWWin): #Basic Point Layout
-    if SeasonUser&SeasonUserWin==True:
+def user_Bet_Points(SeasonUserWin,UserScore,WeeklyUserWin,SeasonUser,WeeklyUser,WeeklyUserGOWWin,SeasonUserGOWWin): #Basic Point Layout
+    if SeasonUser&SeasonUserWin:
         UserScore+1
     else:
         UserScore
 
-    if WeeklyUser&WeeklyUserWin==True:
+    if WeeklyUser&WeeklyUserWin:
         UserScore+1
     else:
         UserScore
 
-    if WeeklyUser & WeeklyUserGOWWin == True:
+    if WeeklyUser & WeeklyUserGOWWin:
         UserScore + 2
     else:
         UserScore
 
-    if SeasonUser & SeasonUserGOWWin == True:
+    if SeasonUser & SeasonUserGOWWin:
         UserScore + 2
     else:
         UserScore
